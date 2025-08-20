7c644a12636cfc8616eaa5e843fab820885a1e76
Shreevathsa05 <bhatshreevathsa17@gmail.com>
2025-08-20 16:47:07 +0530

    Disclaimer add

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -3,8 +3,16 @@ import Dashboard from '../Components/Dashboard'
 
 function Analytics() {
   return (
-    <div>
-      <Dashboard/>
+    <div className="p-6">
+
+      {/* Dashboard Component */}
+      <Dashboard />
+
+      {/* Page Description */}
+      <p className="mb-6 text-gray-600 border-2 border-black rounded-lg p-2">
+        Explore this demo analytics dashboard, designed to showcase key metrics
+        and provide a foundation for prototyping and experimentation.
+      </p>
     </div>
   )
 }
